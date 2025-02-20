e8b5dcec0c60f3663c48e5541c9ccb1305540f4f
DevHieu <hieudd2090@gmail.com>
2025-02-20 09:37:21 +0700

    Remove backend folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#20321e",
+    },
+    secondary: {
+      main: "#466245",
+    },
+  },
+
+  typography: {
+    fontFamily: ["Protest Revolution", "sans-serif"].join(","),
+  },
+});
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+);
